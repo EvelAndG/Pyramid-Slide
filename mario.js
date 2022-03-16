315d0396b0fa741de99a0c59799318259ad4e166
Gage Eveland <42362153+EvelAndG@users.noreply.github.com>
2022-03-16 14:55:22 +0000

    Start Point

--- mario.js
+++ mario.js
@@ -1,56 +1,103 @@
 
 
-var userSelection = document.querySelector('Select');
-userSelection.addEventListener("change", printPyramid);
+var heightElem = document.getElementById("height");
+var formElem = document.getElementById("draw-form");
 
-var inputHeight = document.getElementById("height");
-inputHeight.addEventListener("input", printPyramid);
+// set a handler function for the form's submission event
+formElem.onsubmit = function(event) {
 
-function printPyramid() {
-    
-    var inputValue = document.getElementById("height").value;
-    x = parseInt(inputValue);
-    return drawPyramid(x);  
+    // QUIZ
+    // what happens if we don't do this?
+    event.preventDefault();
 
-}
+    // QUIZ
+    // what happens if we don't do this?
+    clearError();
 
- function drawPyramid(height) {    
+    // figure out the height the user typed
+    heightStr = heightElem.value;
 
-	  var symbolInput = document.getElementById("symbols").value;
-		let brick = symbolInput;
-    
-    // removes existing pyramid
-    var pyramidDiv = document.getElementById("pyramid");
-    while (pyramidDiv.firstChild) {
-        pyramidDiv.removeChild(pyramidDiv.firstChild);
+    // TODO 1
+    // if they didn't type anything at all, give a different error message,
+    // something like "Please provide a height"
+
+
+    // convert the string to an int
+    height = parseInt(heightStr);
+
+    // if the height is not-a-number, yell at them and exit early
+    // TODO 2
+    // negative numbers and zero should also be rejected here
+    if (isNaN(height)) {
+        displayError("That's not a valid height.");
+        return;
     }
 
+    // if the height is absurdly tall, yell at them and exit early
+    var tooTall = 100;
+    if (height > tooTall) {
+        displayError("Are you cray? I can't build a pyramid that tall.");
+        return;
+    }
 
+    // draw pyramid with the specified height
+    drawPyramid(height);
+}
+
+
+/**
+ * displayError
+ *
+ * Displays an error message on the text input, and colors it red
+ */
+function displayError(message) {
+    heightElem.className = "invalid-field";
+    document.querySelector(".error-message").innerHTML = message;
+}
+
+
+/*
+ * clearError
+ *
+ * Undisplays the error message and removes the red CSS style
+ */
+function clearError(message) {
+    // TODO 3
+    // implement this function.
+}
+
+
+
+/**
+ * drawPyramid
+ *
+ * Renders, in the HTML document, a Mario pyramid of the specified height
+ */
+function drawPyramid(height) {
+
+    // first, clear the old content
+    document.getElementById("pyramid").innerHTML = "";
+
+    // for each row....
     for (var row = 0; row < height; row++) {
 
+        // figure out number of bricks and spaces
         var numBricks = row + 2;
         var numSpaces = height - row - 1;
 
-    
+        // build up a string for this row
         var rowStr = "";
         for (var i = 0; i < numSpaces; i++) {
-            rowStr += ".";
+            var spaceChar = "&nbsp"; // this is the HTML encoding for a space " "
+            rowStr += spaceChar;
         }
         for (var i = 0; i < numBricks; i++) {
-            rowStr += brick;
-
+            rowStr += "#";
         }
 
-        textElem = document.createTextNode(rowStr);
-
+        // make a <p> element for this row, and insert it into the #pyramid container
         rowElem = document.createElement("p");
-        rowElem.appendChild(textElem);
-
-
+        rowElem.innerHTML = rowStr;
         document.getElementById("pyramid").appendChild(rowElem);
-
-				document.getElementById("sliderValue").innerHTML = height;
-			
     }
 }
-
